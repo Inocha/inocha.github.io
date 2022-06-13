427febe28c53ed25be6616aeffcc1d0a176972d1
Inocha <mirjana.vujkov@gmail.com>
2022-06-13 15:15:23 -0700

    Fix problems
    Fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function start (){
     //$("body").css("background-color", "pink");
-
+    $("menu a:first").click();
 }
 
 $(window).on("load", start);
@@ -10,8 +10,10 @@ function loadpage(e){
     e.preventDefault();
     $("#menu a.active").removeClass("active");
     $(this).addClass("active");
-    
-    $("content").load("about.html");
+
+
+    var href = $(this).attr("href");
+    $("#content").load("href");
 
 
 }
